523bc2c774ea881b34942a0ec3cd2bd8e94fa6d8
xtlx2000 <lvpengcheng6300@gmail.com>
2014-03-27 19:54:38 +0800

    update mm2

---
+++ mm2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "slab.h"
+#include "buddy.h"
+#include "pm.h"
+#include "vm.h"
+
+
+#include "vfs.h"
+#include "ext2.h"
+
+
+#include "cache.h"
+#include "buffer.h"
+
+
+
+
+int main()
+{
+	printf("hello\n");
+
+	return 0;
+}
