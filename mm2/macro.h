e748017965c964540540eb439f296c0c791b5d17
xtlx2000 <lvpengcheng6300@gmail.com>
2014-04-09 09:46:54 +0800

    update mm2

--- mm2/macro.h
+++ mm2/macro.h
@@ -9,6 +9,9 @@
 #define atomic_t int
 #define u16 unsigned short
 #define u32 unsigned int
+#define u64 unsigned long
+
+
 #define s8  unsigned int
 
 
@@ -16,6 +19,12 @@
 
 #define pgoff_t int
 #define gfp_t int
+
+#define loff_t int
+
+#define ssize_t unsigned long
+#define size_t unsigned long
+
 
 
 
@@ -36,6 +45,13 @@
 #define DEBUG 1
 #define PRINT_DEBUG(fmt, args...)    if(DEBUG)printf(fmt, ## args)
 
+
+
+struct __wait_queue_head {
+    spinlock_t lock;
+    struct list_head task_list;
+};  
+typedef struct __wait_queue_head wait_queue_head_t;
 
 
 
--- mm2/vfs.h
+++ mm2/vfs.h
@@ -1,6 +1,9 @@
 #ifndef _VFS_H_
 #define _VFS_H_
 
+#include "device.h"
+
+
 struct path {                                                                                               
     struct vfsmount *mnt;
     struct dentry *dentry;
@@ -13,6 +16,32 @@ struct fs_struct {
     int umask;
     struct path root, pwd;
 };
+
+
+#define __FD_SETSIZE    1024
+
+
+typedef struct {
+    unsigned long fds_bits [__FD_SETSIZE];
+} __kernel_fd_set;
+
+
+
+typedef __kernel_fd_set     fd_set;
+
+
+struct fdtable {                                                                                            
+    unsigned int max_fds;
+    struct file ** fd;      /* current fd array */ 
+    fd_set *close_on_exec;
+    fd_set *open_fds;
+    struct list_head rcu;
+    struct fdtable *next;
+};
+
+
+
+#define NR_OPEN_DEFAULT 1024
 
 
 /*  
@@ -28,12 +57,29 @@ struct files_struct {
   /*
    * written part on a separate cache line in SMP
    */
-    spinlock_t file_lock ____cacheline_aligned_in_smp;
+    //spinlock_t file_lock ____cacheline_aligned_in_smp;
     int next_fd;
-    struct embedded_fd_set close_on_exec_init;
-    struct embedded_fd_set open_fds_init;
+    //struct embedded_fd_set close_on_exec_init;
+    //struct embedded_fd_set open_fds_init;
     struct file * fd_array[NR_OPEN_DEFAULT];
 };
+
+
+/*
+ * "quick string" -- eases parameter passing, but more importantly
+ * saves "metadata" about the string (ie length and the hash).
+ *
+ * hash comes first so it snuggles against d_parent in the
+ * dentry.
+ */ 
+struct qstr {
+    unsigned int hash;
+    unsigned int len;
+    const unsigned char *name;
+};
+
+
+#define DNAME_INLINE_LEN_MIN 1024
 
 
 
@@ -59,7 +105,7 @@ struct dentry {
 	 */  
 	union {  
 		struct list_head d_child;/*对于目录而言，用于统一父目录中的目录项链表的指针*/	/* child of parent list */	
-		struct rcu_head d_rcu;	
+		struct list_head d_rcu;	
 	} d_u;	
 	struct list_head d_subdirs;/*对目录，子目录项链表的指针*/	 /* our children */  
 	struct list_head d_alias;/*用于与同意索引节点相关的目录项链表的指针*/	/* inode alias list */	
@@ -73,6 +119,25 @@ struct dentry {
 
 
 
+#define MAX_NESTED_LINKS 1024
+
+
+struct nameidata {                                                                                          
+    struct path path;
+    struct qstr last;
+    unsigned int    flags;
+    int     last_type;
+    unsigned    depth;
+    char *saved_names[MAX_NESTED_LINKS + 1];
+
+    /* Intent data */
+    union {
+        //struct open_intent open;
+    } intent;
+}; 
+
+
+
 struct dentry_operations {
     int (*d_revalidate)(struct dentry *, struct nameidata *);
     int (*d_hash) (struct dentry *, struct qstr *);//计算hash值
@@ -81,6 +146,145 @@ struct dentry_operations {
     void (*d_release)(struct dentry *);
     void (*d_iput)(struct dentry *, struct inode *);//从一个不再使用的dentry对象中释放inode（在默认情况下，将inode的使用计数器减1，计数器到达0时，将inode从各种链表中移除）。
     char *(*d_dname)(struct dentry *, char *, int);
+};
+
+
+
+
+struct mnt_namespace {                                                                                      
+    atomic_t        count;
+    struct vfsmount *   root;
+    struct list_head    list;
+    wait_queue_head_t poll;
+    int event;
+};
+
+
+/* is there a better place to document function pointer methods? */
+/**
+ * ki_retry -   iocb forward progress callback
+ * @kiocb:  The kiocb struct to advance by performing an operation.
+ *
+ * This callback is called when the AIO core wants a given AIO operation
+ * to make forward progress.  The kiocb argument describes the operation
+ * that is to be performed.  As the operation proceeds, perhaps partially,
+ * ki_retry is expected to update the kiocb with progress made.  Typically
+ * ki_retry is set in the AIO core and it itself calls file_operations
+ * helpers.
+ *
+ * ki_retry's return value determines when the AIO operation is completed
+ * and an event is generated in the AIO event ring.  Except the special
+ * return values described below, the value that is returned from ki_retry
+ * is transferred directly into the completion ring as the operation's
+ * resulting status.  Once this has happened ki_retry *MUST NOT* reference
+ * the kiocb pointer again.
+ *
+ * If ki_retry returns -EIOCBQUEUED it has made a promise that aio_complete()
+ * will be called on the kiocb pointer in the future.  The AIO core will
+ * not ask the method again -- ki_retry must ensure forward progress.
+ * aio_complete() must be called once and only once in the future, multiple
+ * calls may result in undefined behaviour.
+ *
+ * If ki_retry returns -EIOCBRETRY it has made a promise that kick_iocb()
+ * will be called on the kiocb pointer in the future.  This may happen
+ * through generic helpers that associate kiocb->ki_wait with a wait
+ * queue head that ki_retry uses via current->io_wait.  It can also happen
+ * with custom tracking and manual calls to kick_iocb(), though that is
+ * discouraged.  In either case, kick_iocb() must be called once and only
+ * once.  ki_retry must ensure forward progress, the AIO core will wait
+ * indefinitely for kick_iocb() to be called.
+ */
+struct kiocb {
+    struct list_head    ki_run_list;
+    unsigned long       ki_flags;
+    int         ki_users;
+    unsigned        ki_key;     /* id of this request */
+
+    struct file     *ki_filp;
+    //struct kioctx       *ki_ctx;    /* may be NULL for sync ops */
+    //int         (*ki_cancel)(struct kiocb *, struct io_event *);
+    ssize_t         (*ki_retry)(struct kiocb *);
+    void            (*ki_dtor)(struct kiocb *);
+
+    union {
+        void      *user;
+        struct task_struct  *tsk;
+    } ki_obj;                                                                       
+
+    u64           ki_user_data;   /* user's data for completion */
+    wait_queue_t        ki_wait;
+    loff_t          ki_pos;
+
+    void            *private;
+    /* State that we remember to be able to restart/retry  */
+    unsigned short      ki_opcode;
+    size_t          ki_nbytes;  /* copy of iocb->aio_nbytes */
+    char            *ki_buf; /* remaining iocb->aio_buf */
+    size_t          ki_left;    /* remaining bytes */
+    struct iovec        ki_inline_vec;  /* inline vector */
+    struct iovec        *ki_iovec;
+    unsigned long       ki_nr_segs;
+    unsigned long       ki_cur_seg;
+
+    struct list_head    ki_list;    /* the aio core uses this
+                         * for cancellation */
+
+    /*
+     * If the aio_resfd field of the userspace iocb is not zero,
+     * this is the underlying file* to deliver event to.
+     */
+    struct file     *ki_eventfd;
+};
+
+
+/*
+ *  Berkeley style UIO structures   -   Alan Cox 1994.
+ *
+ *      This program is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU General Public License
+ *      as published by the Free Software Foundation; either version
+ *      2 of the License, or (at your option) any later version.
+ */
+
+struct iovec                                                                                                
+{
+    void *iov_base;  /* BSD uses caddr_t (1003.1g requires void *) */
+    u32 iov_len; /* Must be size_t (1003.1g) */
+};
+
+
+/*
+ * NOTE:
+ * read, write, poll, fsync, readv, writev, unlocked_ioctl and compat_ioctl
+ * can be called without the big kernel lock held in all filesystems.
+ */ 
+struct file_operations {
+    loff_t (*llseek) (struct file *, loff_t, int);
+    ssize_t (*read) (struct file *, char *, size_t, loff_t *);
+    ssize_t (*write) (struct file *, const char *, size_t, loff_t *);
+    ssize_t (*aio_read) (struct kiocb *, const struct iovec *, unsigned long, loff_t);
+    ssize_t (*aio_write) (struct kiocb *, const struct iovec *, unsigned long, loff_t);
+    int (*readdir) (struct file *, void *, filldir_t);
+    unsigned int (*poll) (struct file *, struct poll_table_struct *);
+    int (*ioctl) (struct inode *, struct file *, unsigned int, unsigned long);
+    long (*unlocked_ioctl) (struct file *, unsigned int, unsigned long);
+    long (*compat_ioctl) (struct file *, unsigned int, unsigned long);
+    int (*mmap) (struct file *, struct vm_area_struct *);
+    int (*open) (struct inode *, struct file *);
+    int (*flush) (struct file *, fl_owner_t id);
+    int (*release) (struct inode *, struct file *);
+    int (*fsync) (struct file *, struct dentry *, int datasync);
+    int (*aio_fsync) (struct kiocb *, int datasync);
+    int (*fasync) (int, struct file *, int);
+    int (*lock) (struct file *, int, struct file_lock *);
+    ssize_t (*sendpage) (struct file *, struct page *, int, size_t, loff_t *, int);
+    unsigned long (*get_unmapped_area)(struct file *, unsigned long, unsigned long, unsigned long, unsigned long);
+    int (*check_flags)(int);
+    int (*dir_notify)(struct file *filp, unsigned long arg);
+    int (*flock) (struct file *, int, struct file_lock *);
+    ssize_t (*splice_write)(struct pipe_inode_info *, struct file *, loff_t *, size_t, unsigned int);
+    ssize_t (*splice_read)(struct file *, loff_t *, struct pipe_inode_info *, size_t, unsigned int);
+    int (*setlease)(struct file *, long, struct file_lock **);
 };
 
 
@@ -135,13 +339,13 @@ struct super_block {
 	loff_t			s_maxbytes;/*文件的最长长度*/  /* Max file size */	
 	struct file_system_type *s_type;/*文件系统类型*/  
 	const struct super_operations	*s_op;/*超级快方法*/  
-	const struct dquot_operations	*dq_op;/*磁盘限额方法*/  
-	const struct quotactl_ops	*s_qcop;/*磁盘限额管理方法*/  
-	const struct export_operations *s_export_op;/*网络文件系统使用的输出方法*/	
+	//const struct dquot_operations	*dq_op;/*磁盘限额方法*/  
+	//const struct quotactl_ops	*s_qcop;/*磁盘限额管理方法*/  
+	//const struct export_operations *s_export_op;/*网络文件系统使用的输出方法*/	
 	unsigned long		s_flags;/*安装标识*/  
 	unsigned long		s_magic;/*文件系统的魔术*/	
 	struct dentry		*s_root;/*文件系统根目录的目录项对象*/	
-	struct rw_semaphore s_umount;/*卸载用的信号量*/  
+	//struct rw_semaphore s_umount;/*卸载用的信号量*/  
 	struct mutex		s_lock;/*超级块信号量*/  
 	int 		s_count;/*引用计数*/  
 	int 		s_need_sync;/*表示对超级快的索引节点进行同步标志*/	
@@ -149,7 +353,7 @@ struct super_block {
 #ifdef CONFIG_SECURITY	
 	void					*s_security;/*指向超级安全数据结构的指针*/	
 #endif	
-	struct xattr_handler	**s_xattr;/*执行超级快扩展数据结构的指针*/	
+	//struct xattr_handler	**s_xattr;/*执行超级快扩展数据结构的指针*/	
   
 	struct list_head	s_inodes;/*所有索引节点的链表*/  /* all inodes */  
 	struct hlist_head	s_anon;/*用于处理网络文件系统的匿名目录项的链表*/	   /*  
@@ -161,9 +365,9 @@ anonymous dentries for (nfs) exporting */
   
 	struct block_device *s_bdev;/*指向块设备驱动程序描述符的指针*/	
 	struct backing_dev_info *s_bdi;/* */  
-	struct mtd_info 	*s_mtd;/**/  
+	//struct mtd_info 	*s_mtd;/**/  
 	struct list_head	s_instances;/*用于指定文件系统类型的超级快对象链表指针*/  
-	struct quota_info	s_dquot;/*磁盘限额的描述符*/	/* Diskquota specific options  
+	//struct quota_info	s_dquot;/*磁盘限额的描述符*/	/* Diskquota specific options  
 */	
   
 	int 		s_frozen;/*冻结文件系统时使用的标志*/  
@@ -171,16 +375,14 @@ anonymous dentries for (nfs) exporting */
   
 	char s_id[32];/*包含超级快的块设备名称*/			   /* Informational name */  
   
-	void			*s_fs_info;/*指向特定文件系统的超级快信息的指针*/	 /* Filesystem	
-private info */  
-	fmode_t 		s_mode;/**/  
+	void			*s_fs_info;/*指向特定文件系统的超级快信息的指针*/	 /* Filesystem private info */  
+	//fmode_t 		s_mode;/**/  
   
 	/* 
 	 * The next field is for VFS *only*. No filesystems have any business 
 	 * even looking at it. You had been warned. 
 	 */  
-	struct mutex s_vfs_rename_mutex;/*当VFS通过目录命名文件时使用的互斥变量*/	 /*  
-Kludge */  
+	//struct mutex s_vfs_rename_mutex;/*当VFS通过目录命名文件时使用的互斥变量*/  
   
 	/* Granularity of c/m/atime in ns. 
 	   Cannot be worse than a second */  
@@ -198,6 +400,21 @@ Kludge */
 	 */  
 	char *s_options;  
 };	
+
+
+struct kstatfs {                                                                                            
+    long f_type;
+    long f_bsize;
+    u64 f_blocks;
+    u64 f_bfree;
+    u64 f_bavail;
+    u64 f_files;
+    u64 f_ffree;
+    //__kernel_fsid_t f_fsid;
+    long f_namelen;
+    long f_frsize;      
+    long f_spare[5];
+};
 
 
 struct super_operations {
@@ -218,13 +435,35 @@ struct super_operations {
     void (*clear_inode) (struct inode *);
     void (*umount_begin) (struct super_block *);
 
-    int (*show_options)(struct seq_file *, struct vfsmount *);
-    int (*show_stats)(struct seq_file *, struct vfsmount *);
+    //int (*show_options)(struct seq_file *, struct vfsmount *);
+    //int (*show_stats)(struct seq_file *, struct vfsmount *);
 #ifdef CONFIG_QUOTA
     ssize_t (*quota_read)(struct super_block *, int, char *, size_t, loff_t);
     ssize_t (*quota_write)(struct super_block *, int, const char *, size_t, loff_t);
 #endif
 };
+
+
+
+struct address_space {
+    struct inode        *host;      /* owner: inode, block_device */
+    struct radix_tree_root  page_tree;  /* radix tree of all pages */
+    spinlock_t      tree_lock;  /* and lock protecting it */
+    unsigned int        i_mmap_writable;/* count VM_SHARED mappings */
+    struct prio_tree_root   i_mmap;     /* tree of private and shared mappings */
+    struct list_head    i_mmap_nonlinear;/*list VM_NONLINEAR mappings */
+    spinlock_t      i_mmap_lock;    /* protect tree, count, list */
+    unsigned int        truncate_count; /* Cover race condition with truncate */
+    unsigned long       nrpages;    /* number of total pages */
+    pgoff_t         writeback_index;/* writeback starts here */
+    const struct address_space_operations *a_ops;   /* methods */
+    unsigned long       flags;      /* error bits/gfp mask */
+    struct backing_dev_info *backing_dev_info; /* device readahead, etc */
+    spinlock_t      private_lock;   /* for use by the address_space */
+    struct list_head    private_list;   /* ditto */
+    struct address_space    *assoc_mapping; /* ditto */
+} __attribute__((aligned(sizeof(long))));
+
 
 
 
@@ -240,26 +479,25 @@ struct inode {
 	unsigned long		i_ino;/*索引节点号*/  
 	atomic_t		i_count;/*引用计数器*/	
 	unsigned int		i_nlink;/*硬链接数目*/	
-	uid_t			i_uid;/*所有者标识符*/	
-	gid_t			i_gid;/*主标识符*/	
+	//uid_t			i_uid;/*所有者标识符*/	
+	//gid_t			i_gid;/*主标识符*/	
 	dev_t			i_rdev;/*实设备标识符*/  
 	u64 		i_version;/*版本号，每次使用后自动增加*/  
 	loff_t			i_size;/*文件的字节数*/  
 #ifdef __NEED_I_SIZE_ORDERED  
 	seqcount_t		i_size_seqcount;  
 #endif	
-	struct timespec 	i_atime;/*上次访问文件的时间*/	
-	struct timespec 	i_mtime;/*上次写文件的时间*/  
-	struct timespec 	i_ctime;/*上次修改索引节点的时间*/	
-	blkcnt_t		i_blocks;/*文件的块数*/  
-	unsigned int		i_blkbits;/*块的位数*/	
-	unsigned short			i_bytes;/*文件最后一个块的字节数*/	
-	umode_t 		i_mode;/**/  
+	//struct timespec 	i_atime;/*上次访问文件的时间*/	
+	//struct timespec 	i_mtime;/*上次写文件的时间*/  
+	//struct timespec 	i_ctime;/*上次修改索引节点的时间*/	
+	u32		i_blocks;/*文件的块数*/  
+	u32		i_blkbits;/*块的位数*/	
+	u16			i_bytes;/*文件最后一个块的字节数*/	
+	u32 		i_mode;/**/  
 	spinlock_t		i_lock;/*保护索引节点一些字段的子璇锁*/   /* i_blocks, i_bytes,  
 maybe i_size */  
-	struct mutex		i_mutex;/**/  
-	struct rw_semaphore i_alloc_sem;/*在直接IO 
-文件操作中避免出现竞争条件的读写信号量*/  
+	//struct mutex		i_mutex;/**/  
+	//struct rw_semaphore i_alloc_sem;/*在直接IO 文件操作中避免出现竞争条件的读写信号量*/  
 	const struct inode_operations	*i_op;/*索引节点的操作*/  
 	const struct file_operations	*i_fop;/*缺省文件操作*/   /* former ->i_op-> default_file_ops */  
 	struct super_block	*i_sb;/*指向超级快对象的指针*/	
@@ -276,10 +514,10 @@ maybe i_size */
 		struct cdev 	*i_cdev;/*指向字符设备驱动的指针*/	
 	};	
   
-	__u32			i_generation;/*索引节点版本号*/  
+	u32			i_generation;/*索引节点版本号*/  
   
 #ifdef CONFIG_FSNOTIFY	
-	__u32			i_fsnotify_mask;/*目录通知事件的位掩码*//* all events this inode  
+	u32			i_fsnotify_mask;/*目录通知事件的位掩码*//* all events this inode  
 cares about */	
 	struct hlist_head	i_fsnotify_mark_entries; /* fsnotify mark entries */  
 #endif	
